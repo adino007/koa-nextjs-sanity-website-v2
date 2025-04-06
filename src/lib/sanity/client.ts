e00e8fa4106656421cb1c05a7aed25216b9ad354
Adam Alakeli <90493664+adino007@users.noreply.github.com>
2025-04-06 15:49:59 -0700

    Update client.ts

--- src/lib/sanity/client.ts
+++ src/lib/sanity/client.ts
@@ -1,14 +1,16 @@
 import { createClient } from 'next-sanity'
-import { apiVersion, dataset, projectId, token } from './env'
+import dev from '@/lib/env'
 
-export const client = createClient({
-	projectId,
-	dataset,
-	apiVersion,
-	token,
-	useCdn: false,
-	perspective: 'published',
+export default createClient({
+	projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+	dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+	apiVersion: '2025-02-19',
+	useCdn: !dev,
 	stega: {
 		enabled: false,
+		studioUrl: '/admin',
 	},
+	perspective: 'published',
+	timeout: 60000, // 60 seconds timeout
+	token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
 })
